7dd169ddb8808cd4caf64f1d21d81aab2487bebf
SaltReeves <160738177+SaltReeves@users.noreply.github.com>
2025-12-26 10:38:45 +0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,30 +22,6 @@ const Footer = () => {
           </div>
         </div>
       </div>
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6585857350615324"
-     crossorigin="anonymous"></script>
-      <script
-  type="text/javascript"
-  src="https://pl28280829.effectivegatecpm.com/34/4c/a3/344ca35c1cf65a133be4402831b5fc25.js"
-></script>
-<script type="text/javascript">
-  atOptions = {
-  	'key' : '358ed44009501c12ea99fe4d15d885be',
-  	'format' : 'iframe',
-  	'height' : 250,
-  	'width' : 300,
-  	'params' : {}
-  };
-</script>
-<script
-  type="text/javascript"
-  src="https://www.highperformanceformat.com/358ed44009501c12ea99fe4d15d885be/invoke.js"
-></script>
-<script
-  async="async"
-  data-cfasync="false"
-  src="https://pl28280844.effectivegatecpm.com/dc1492619050697ee754228485b103a6/invoke.js"
-></script>
 <div id="container-dc1492619050697ee754228485b103a6"></div>
     </footer>
   );
